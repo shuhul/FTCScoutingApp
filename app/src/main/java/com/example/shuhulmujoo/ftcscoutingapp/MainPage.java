56217e50986e3c57d24bda37870cb43eb794df24
shuhulmujoo <shuhulmujoo@gmail.com>
2019-06-28 12:40:13 -0700

    finished main page

--- app/src/main/java/com/example/shuhulmujoo/ftcscoutingapp/MainPage.java
+++ app/src/main/java/com/example/shuhulmujoo/ftcscoutingapp/MainPage.java
@@ -1,7 +1,13 @@
 package com.example.shuhulmujoo.ftcscoutingapp;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+import android.widget.TextView;
+
 
 public class MainPage extends AppCompatActivity {
 
@@ -9,5 +15,6 @@ public class MainPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
+
     }
 }
